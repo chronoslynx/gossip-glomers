98d52d524f6a0e5af11790136a6d41a60689e1e7
Tim Deeb-Swihart <tim@deebswih.art>
2023-03-05 11:20:25 -0800

    Add kafka (challenge 5) code and new Node abstraction
    I've noticed I'm redoing a lot of synchronization crap for each
    challenge, so I've abstracted it out into an actor-like helper

---
+++ kafka/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+
+	"glomers/node"
+)
+
+type Send struct {
+	Key string `json:"key"`
+	Msg int    `json:"msg"`
+}
+
+type SendOk struct {
+	MsgType string `json:"type"`
+	Offset  int    `json:"offset"`
+}
+
+// poll, commit_offsets, list_committed_offsets_ok
+type Offsets struct {
+	MsgType string         `json:"type"`
+	Offsets map[string]int `json:"offsets"`
+}
+
+type PollOK struct {
+	MsgType string              `json:"type"`
+	Msgs    map[string][][2]int `json:"msgs"`
+}
+
+type ListCommittedOffsets struct {
+	Keys []string `json:"keys"`
+}
+
+var coffOk = json.RawMessage(`{"type":"commit_offsets_ok"}`)
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	n := node.New(ctx, nil)
+
+	go func() {
+		sends, sendOK := node.Handle[Send, SendOk](n, "send")
+		polls, pollOK := node.Handle[Offsets, PollOK](n, "poll")
+		commits, commitOK := node.Handle[Offsets, json.RawMessage](n, "commit_offsets")
+		listReqs, lists := node.Handle[ListCommittedOffsets, Offsets](n, "list_committed_offsets")
+
+		// State
+		keys := make(map[string][]int)
+		committed := make(map[string]int)
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case s := <-sends:
+				keys[s.Key] = append(keys[s.Key], s.Msg)
+				node.Reply(n, sendOK, SendOk{
+					MsgType: "send_ok",
+					Offset:  len(keys[s.Key]),
+				})
+
+			case poll := <-polls:
+				resp := PollOK{MsgType: "poll_ok", Msgs: make(map[string][][2]int)}
+				for k, vs := range keys {
+					for i, v := range vs[poll.Offsets[k]:] {
+						resp.Msgs[k] = append(resp.Msgs[k], [2]int{poll.Offsets[k] + i, v})
+					}
+				}
+				node.Reply(n, pollOK, resp)
+			case o := <-commits:
+				for k, v := range o.Offsets {
+					committed[k] = v
+				}
+				node.Reply(n, commitOK, coffOk)
+			case <-listReqs:
+				resp := Offsets{MsgType: "list_committed_offsets_ok", Offsets: make(map[string]int)}
+				for k, offs := range committed {
+					resp.Offsets[k] = offs
+				}
+				node.Reply(n, lists, resp)
+
+			}
+		}
+	}()
+
+	if err := n.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ node/node.go
@@ -0,0 +1,80 @@
+// Wrap maelstrom nodes with some helpers
+package node
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+var initOk = json.RawMessage(`{"type":"init_ok"}`)
+var topoOk = json.RawMessage(`{"type":"topology_ok"}`)
+
+type Node struct {
+	ctx  context.Context
+	n    *maelstrom.Node
+	init func(n *maelstrom.Node) error
+}
+
+func New(ctx context.Context, init func(n *maelstrom.Node) error) Node {
+	n := Node{
+		ctx:  ctx,
+		n:    maelstrom.NewNode(),
+		init: init,
+	}
+
+	if init != nil {
+		n.n.Handle("init", func(m maelstrom.Message) error {
+			if err := init(n.n); err != nil {
+				return err
+			}
+			return n.n.Reply(m, initOk)
+		})
+	}
+	// I don't care about topology messages either
+	n.n.Handle("topology", func(msg maelstrom.Message) error {
+		return n.n.Reply(msg, topoOk)
+	})
+
+	return n
+}
+
+func (n Node) Run() error {
+	return n.n.Run()
+}
+
+func Handle[In any, Out any](n Node, kind string) (<-chan In, chan<- Out) {
+	inCh := make(chan In, 1)
+	outCh := make(chan Out, 1)
+
+	done := n.ctx.Done()
+	n.n.Handle(kind, func(msg maelstrom.Message) error {
+		var in In
+		if err := json.Unmarshal(msg.Body, &in); err != nil {
+			return fmt.Errorf("failed to unmarshal %s message as %T: %w", kind, in, err)
+		}
+		select {
+		case <-done:
+			return nil
+		case inCh <- in:
+			select {
+			case <-done:
+				return nil
+			case reply := <-outCh:
+				return n.n.Reply(msg, reply)
+			}
+		}
+	})
+
+	return inCh, outCh
+}
+
+func Reply[T any](n Node, tCh chan<- T, v T) {
+	select {
+	case <-n.ctx.Done():
+		return
+	case tCh <- v:
+		return
+	}
+}
